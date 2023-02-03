054c7bf8a5658c6a5f38c4449c0dffb80dcc0a50
RobusV <rverplanke@gmail.com>
2023-02-03 21:01:42 +0100

    make remove icon work

--- script/script.js
+++ script/script.js
@@ -4,42 +4,51 @@ const list = document.getElementById("list");
 const items = document.getElementById("items");
 
 // Invoerveld
-
 const inputField = document.createElement("input");
 inputField.classList.add("input-field");
 inputField.setAttribute('type', 'text');
 
 // Verzendknop
-
 const inputButton = document.createElement("button");
 inputButton.innerText = "Voeg toe";
 inputButton.addEventListener("click", () => addItem());
 
 // Functie om item aan lijst toe te voegen
-
 function addItem(){
+
+    // Maak verwijder-icoon
+    let trashIcon = document.createElement("IMG");
+    trashIcon.setAttribute('src', 'img/trashcan.png');
+    trashIcon.addEventListener("click", () => removeItem(listItem));
+
+    // Maak checkbox
     let checkbox = document.createElement("input");
     checkbox.setAttribute('type', 'checkbox');
     checkbox.setAttribute('id', 'check');
     
-    let listItem = document.createElement("li");
-    listItem.append(checkbox, inputField.value);
-    
-    checkbox.addEventListener("change", function(e){
+    // Controleert of de checkbox 'aan' of 'uit' staat. Bij verandering wissel van class
+    checkbox.addEventListener("change", function(e) {
         if (this.checked) {
             listItem.classList.add("checked");
-            console.log("i am checked " + listItem.classList);
         } else {
-            listItem.classList.remove("checked")
-            console.log("i am not checked " + listItem.classList);
+            listItem.classList.remove("checked");
         }
     });
 
-    
+    let listItem = document.createElement("li");
+    listItem.append(trashIcon, checkbox, inputField.value);
     
     items.append(listItem);
 };
 
-// Voeg samen
+// Functie om item van lijst te verwijderen
+function removeItem(item){
+    let confirmRemove = confirm("Verwijderen?")
+    if (confirmRemove == true){
+    item.innerHTML ="";
+    }
+};
 
+
+// Voeg samen
 input.append(inputField, inputButton);
